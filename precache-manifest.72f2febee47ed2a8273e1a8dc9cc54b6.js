cde5419220e18d39dcd1bbb150c0797b4a8bd669
Carlos Alba <soradev4@gmail.com>
2019-11-27 14:29:40 -0800

    Updates

---
+++ precache-manifest.72f2febee47ed2a8273e1a8dc9cc54b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "866305200679205c6dcd8cbbf151c809",
+    "url": "/meme_generator/index.html"
+  },
+  {
+    "revision": "b65543e7338a6b7788e5",
+    "url": "/meme_generator/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "1ec6fb23de8129d6ad60",
+    "url": "/meme_generator/static/js/2.e7f57270.chunk.js"
+  },
+  {
+    "revision": "b65543e7338a6b7788e5",
+    "url": "/meme_generator/static/js/main.87020c00.chunk.js"
+  },
+  {
+    "revision": "56fff3a2c9ce0c034712",
+    "url": "/meme_generator/static/js/runtime-main.c639a6cf.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/meme_generator/precache-manifest.72f2febee47ed2a8273e1a8dc9cc54b6.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/meme_generator/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
